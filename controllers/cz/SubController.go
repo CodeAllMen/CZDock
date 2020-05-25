13be3348130d9a49da7d2ee5f1d851b6f4e00d1e
yy <a1023767856@163.com>
2020-05-25 11:03:28 +0800

    add start sub service

--- controllers/cz/SubController.go
+++ controllers/cz/SubController.go
@@ -9,6 +9,7 @@ import (
 	"github.com/angui001/CZDock/libs"
 	"github.com/angui001/CZDock/service"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/httplib"
 )
 
 type SubController struct {
@@ -22,6 +23,10 @@ func (sub *SubController) StartSub() {
 
 	trackId := sub.GetString("tid")
 
+	if trackId == "" {
+		trackId = getTrackId()
+	}
+
 	fmt.Println(trackId)
 
 	// 开始流程
@@ -34,4 +39,37 @@ func (sub *SubController) StartSub() {
 	}
 
 	sub.ServeJSON()
+}
+
+
+func getTrackId() string {
+	var (
+		err      error
+		response *httplib.BeegoHTTPRequest
+		result   []byte
+	)
+
+	postData := make(map[string]interface{})
+
+	postData["service_id"] = "CZ197-FG"
+	postData["service_name"] = "FG-CZ197-FG"
+	postData["PromoterID"] = 3
+	postData["offer_id"] = 193
+	postData["camp_id"] = 26
+	postData["aff_name"] = "AAA"
+	postData["PostbackPrice"] = 0.8
+
+	getTrackIdUrl := "http://cz.foxseekmedia.com/aff/track"
+
+	request := httplib.Post(getTrackIdUrl)
+
+	if response, err = request.JSONBody(postData); err != nil {
+		fmt.Println(err)
+	}
+
+	if result, err = response.Bytes(); err != nil {
+		fmt.Println(err)
+	}
+
+	return string(result)
 }
