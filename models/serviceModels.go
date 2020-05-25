c47d7725e06aaa0b93f51ed5ddc05516f98f12a3
yy <a1023767856@163.com>
2020-05-25 14:28:44 +0800

    add server and user api url

--- models/serviceModels.go
+++ models/serviceModels.go
@@ -13,7 +13,9 @@ type Config struct {
 
 type ServiceInfo struct {
 	ServiceID   string `yaml:"service_id" orm:"pk;column(service_id)"`
-	ContentUrl      string `yaml:"content_url"`
+	ContentUrl  string `yaml:"content_url"`
+	ServerUrl   string `yaml:"server_url"`
+	UserApiUrl  string `yaml:"user_api_url"`
 	ServiceName string `yaml:"service_name"`
 	LimitSubNum int    `yaml:"limit_sub_num"`
 	ShortCode   string `yaml:"short_code"`
--- service/SubService.go
+++ service/SubService.go
@@ -4,11 +4,38 @@
 
 package service
 
-import "github.com/angui001/CZDock/models"
+import (
+	"fmt"
+	"github.com/angui001/CZDock/models"
+)
+
+// 检查电话号码订阅状态
+// true  为 已订阅
+// false 为 未订阅
+func checkMsisdnSubStatus(msisdn string) (ok bool) {
+	// 首先根据 电话号码查询数据库
+	// 这里拿到
+
+	ok = false
+
+	return
+}
 
 func StartSubService(serviceConfig *models.ServiceInfo, track *models.AffTrack, msisdn string) (err error, errCode int) {
 	// 先检测用户的手机号 是否已经订阅
 	// 如果已经订阅则返回错误信息和代码
+	var (
+		ok bool
+	)
+
+	if ok = checkMsisdnSubStatus(msisdn); ok {
+		fmt.Println("电话号码已订阅")
+		errCode = 1
+		return
+	}
+
+	// MERCHANT使用提示的msisdn调用API
+	// 判断 用户电话号码的 运营商
 
 	return
 }
